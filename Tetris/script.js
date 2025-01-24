9961ec14496826fda06f640c183ce6644ac0b442
Rediet-Geremew <140042242+Rediet-Geremew@users.noreply.github.com>
2025-01-24 09:17:34 +0300

    Update script.js

--- Tetris/script.js
+++ Tetris/script.js
@@ -1,1 +1,62 @@
+let canvas = document.querySelector("#tetris");
+let scoreboard = document.querySelector("h2");
+let startButton = document.querySelector("#startBtn");
+let ctx = canvas.getContext("2d");
+ctx.scale(30, 30);
 
+const SHAPES = [
+  [
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+  ],
+  [
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0],
+  ],
+  [
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 1],
+  ],
+  [
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0],
+  ],
+  [
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 0, 0],
+  ],
+  [
+    [1, 1, 1],
+    [0, 1, 0],
+    [0, 0, 0],
+  ],
+  [
+    [1, 1],
+    [1, 1],
+  ],
+];
+
+const COLORS = [
+  "#fff",
+  "#9b5fe0",
+  "#16a4d8",
+  "#60dbe8",
+  "#8bd346",
+  "#efdf48",
+  "#f9a52c",
+  "#d64e12",
+];
+
+const ROWS = 20;
+const COLS = 10;
+
+let grid = generateGrid();
+let fallingPieceObj = null;
+let score = 0;
+let gameInterval = null;
