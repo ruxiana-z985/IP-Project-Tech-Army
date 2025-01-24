155c67499c8ae68ac8f0b13c6d7e3a787dddeb1d
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-01-24 09:33:10 +0300

    script.js

--- Tetris/script.js
+++ Tetris/script.js
@@ -134,65 +134,109 @@ function renderPiece() {
     }
   }
 }
-let canvas = document.querySelector("#tetris");
-let scoreboard = document.querySelector("h2");
-let startButton = document.querySelector("#startBtn");
-let ctx = canvas.getContext("2d");
-ctx.scale(30, 30);
+function moveDown() {
+  if (!collision(fallingPieceObj.x, fallingPieceObj.y + 1))
+    fallingPieceObj.y += 1;
+  else {
+    let piece = fallingPieceObj.piece;
+    for (let i = 0; i < piece.length; i++) {
+      for (let j = 0; j < piece[i].length; j++) {
+        if (piece[i][j] == 1) {
+          let p = fallingPieceObj.x + j;
+          let q = fallingPieceObj.y + i;
+          grid[q][p] = fallingPieceObj.colorIndex;
+        }
+      }
+    }
+    if (fallingPieceObj.y == 0) {
+      alert("Game Over!");
+      grid = generateGrid();
+      score = 0;
+      clearInterval(gameInterval); // Stop the game on Game Over
+      gameInterval = null;
+      startButton.style.display = "block"; // Show the start button again
+    }
+    fallingPieceObj = null;
+  }
+  renderGame();
+}
 
-const SHAPES = [
-  [
-    [0, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 0, 0],
-  ],
-  [
-    [0, 1, 0],
-    [0, 1, 0],
-    [1, 1, 0],
-  ],
-  [
-    [0, 1, 0],
-    [0, 1, 0],
-    [0, 1, 1],
-  ],
-  [
-    [1, 1, 0],
-    [0, 1, 1],
-    [0, 0, 0],
-  ],
-  [
-    [0, 1, 1],
-    [1, 1, 0],
-    [0, 0, 0],
-  ],
-  [
-    [1, 1, 1],
-    [0, 1, 0],
-    [0, 0, 0],
-  ],
-  [
-    [1, 1],
-    [1, 1],
-  ],
-];
+function moveLeft() {
+  if (!collision(fallingPieceObj.x - 1, fallingPieceObj.y))
+    fallingPieceObj.x -= 1;
+  renderGame();
+}
 
-const COLORS = [
-  "#fff",
-  "#9b5fe0",
-  "#16a4d8",
-  "#60dbe8",
-  "#8bd346",
-  "#efdf48",
-  "#f9a52c",
-  "#d64e12",
-];
+function moveRight() {
+  if (!collision(fallingPieceObj.x + 1, fallingPieceObj.y))
+    fallingPieceObj.x += 1;
+  renderGame();
+}
 
-const ROWS = 20;
-const COLS = 10;
+function rotate() {
+  let rotatedPiece = [];
+  let piece = fallingPieceObj.piece;
+  for (let i = 0; i < piece.length; i++) {
+    rotatedPiece.push([]);
+    for (let j = 0; j < piece[i].length; j++) {
+      rotatedPiece[i].push(0);
+    }
+  }
+  for (let i = 0; i < piece.length; i++) {
+    for (let j = 0; j < piece[i].length; j++) {
+      rotatedPiece[i][j] = piece[j][i];
+    }
+  }
 
-let grid = generateGrid();
-let fallingPieceObj = null;
+  for (let i = 0; i < rotatedPiece.length; i++) {
+    rotatedPiece[i] = rotatedPiece[i].reverse();
+  }
+  if (!collision(fallingPieceObj.x, fallingPieceObj.y, rotatedPiece))
+    fallingPieceObj.piece = rotatedPiece;
+  renderGame();
+}
+
+function collision(x, y, rotatedPiece) {
+  let piece = rotatedPiece || fallingPieceObj.piece;
+  for (let i = 0; i < piece.length; i++) {
+    for (let j = 0; j < piece[i].length; j++) {
+      if (piece[i][j] == 1) {
+        let p = x + j;
+        let q = y + i;
+        if (p >= 0 && p < COLS && q >= 0 && q < ROWS) {
+          if (grid[q][p] > 0) {
+            return true;
+          }
+        } else {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+function renderGame() {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      ctx.fillStyle = COLORS[grid[i][j]];
+      ctx.fillRect(j, i, 1, 1);
+    }
+  }
+  renderPiece();
+}
+
+document.addEventListener("keydown", function (e) {
+  let key = e.key;
+  if (key == "ArrowDown") {
+    moveDown();
+  } else if (key == "ArrowLeft") {
+    moveLeft();
+  } else if (key == "ArrowRight") {
+    moveRight();
+  } else if (key == "ArrowUp") {
+    rotate();
+  }
+});
 let score = 0;
 let gameInterval = null;
